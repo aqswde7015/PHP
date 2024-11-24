8ebe13136b221a0fc0624f63b5e18c4acbebf2e3
Ph11 <phongphu7015@gmail.com>
2024-11-25 04:11:02 +0700

    dda them crud

--- products.php
+++ products.php
@@ -4,4 +4,11 @@
     ['name' => 'Sản phẩm 2', 'price' => '2000'],
     ['name' => 'Sản phẩm 3', 'price' => '3000']
  ];
+ $csvw = fopen('data.csv','w');
+    
+    foreach($products as $product){
+        fputcsv($csvw,$product);
+    }
+
+ fclose($csvw);
  ?>
